b602d9a92d715103e33550a5e519339e90155320
Administrator <hello@lzlmall.com>
2018-06-27 18:53:00 +0800

    说明提交文档

--- framework/src/main/java/com/lzlmall/b2b/app/vendor/framework/base/BaseEvent.java
+++
@@ -1,9 +0,0 @@
-package com.lzlmall.b2b.app.vendor.framework.base;
-
-/**
- * Created by Administrator on 2018/4/18.
- * 事件的基类
- */
-
-public class BaseEvent {
-}
---
+++ framework/src/main/java/com/xsgzt/framework/base/BaseEvent.java
@@ -0,0 +1,9 @@
+package com.xsgzt.framework.base;
+
+/**
+ * Created by Administrator on 2018/4/18.
+ * 事件的基类
+ */
+
+public class BaseEvent {
+}
